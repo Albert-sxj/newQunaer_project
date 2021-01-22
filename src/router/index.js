48f742750da3cf5d18a43f403c5ecc48ca755a75
Albert-sxj <15900697165@163.com>
2021-01-22 16:04:44 +0800

    新增page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/views/home/Home.vue'
+import page from '@/views/page/page.vue'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 
 Vue.use(VueRouter)
@@ -8,7 +9,12 @@ Vue.use(VueAwesomeSwiper)
 const routes = [{
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+  },
+  {
+    path: '/page',
+    name: 'page',
+    component: page,
   },
   // {
   //   path: '/about',
---
+++ src/views/page/page.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    111111
+    <Header msg="Welcome to Your Vue.js App"/>
+    <home-swiper></home-swiper>
+    <home-icon></home-icon>
+    <home-recommend></home-recommend>
+    <home-weekend></home-weekend>
+  </div>
+</template>
+<script>
+import Header from '@/views/home/components/Header.vue'
+import HomeSwiper from '@/views/home/components/Swiper.vue'
+import HomeIcon from '@/views/home/components/Icon.vue'
+import HomeRecommend from '@/views/home/components/Recommend.vue'
+import HomeWeekend from '@/views/home/components/Weekend.vue'
+export default {
+  name: 'page',
+  components: {
+    Header,
+    HomeSwiper,
+    HomeIcon,
+    HomeRecommend,
+    HomeWeekend
+  }
+}
+</script>
+<style scoped>
+</style>
